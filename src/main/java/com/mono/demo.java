74204dba685b776e343d147a9f87dd3cac0194d0
GOD0H <92855471+GOD0H@users.noreply.github.com>
2022-09-04 21:30:24 +0800

    ssh bower

--- src/main/java/com/mono/demo.java
+++ src/main/java/com/mono/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("pushhhhhhh111111");
         System.out.println("push222222222");
+        System.out.println("444444444444444");
     }
 }
