e1898af1c7c8a63b0aa7a09d7af078236ab9b42d
GOD0H <92855471+GOD0H@users.noreply.github.com>
2022-09-04 21:28:30 +0800

    pull test

--- src/main/java/com/mono/demo.java
+++ src/main/java/com/mono/demo.java
@@ -2,12 +2,6 @@ package com.mono;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("hotfix");
-        System.out.println("hotfix");
-        System.out.println("hotfix");
-        System.out.println("hotfix");
-        System.out.println("master test!!!");
-        System.out.println("hotfix test1");
         System.out.println("pushhhhhhh111111");
         System.out.println("push222222222");
     }
