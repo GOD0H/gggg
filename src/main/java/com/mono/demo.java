3c5445db66a0bd706b51b784505ff7429f202045
mono <mono@GodHao.com>
2022-09-04 21:00:57 +0800

    hotfix version1

--- src/main/java/com/mono/demo.java
+++ src/main/java/com/mono/demo.java
@@ -2,9 +2,9 @@ package com.mono;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("hello git!!!");
-        System.out.println("hello git2222");
-        System.out.println("hello git33333");
-        System.out.println("hello git44444");
+        System.out.println("hotfix");
+        System.out.println("hotfix");
+        System.out.println("hotfix");
+        System.out.println("hotfix");
     }
 }
